225402f2eb22f0967ca6b0c696038a362f01e7ca
Piyush Maheshwari <maheshwaripiyush@live.in>
2020-08-19 17:51:32 +0530

    Minor changes, added comments

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 # Monk
 import os
 import sys
-sys.path.append("monk_v1/")
 
-# For Keras backend
+# For PyTorch backend
 from monk.pytorch_prototype import prototype
 
 # Flask utils
@@ -14,7 +13,7 @@ from gevent.pywsgi import WSGIServer
 # Define a flask app
 app = Flask(__name__)
 
-# Load the trained models
+# For loading the trained models
 make = None
 model = None
 year = None
@@ -24,6 +23,7 @@ def load_models():
     global model
     global year
 
+    # Load the trained models in infer mode
     make = prototype(verbose=0)
     make.Prototype("Make_classifier", "inception-v3-pytorch", eval_infer=True)
 
@@ -40,19 +40,18 @@ def model_predict(img_path):
     model_preds = model.Infer(img_name=img_path, return_raw=False)
     model_class = model_preds['predicted_class'].replace("_", " ")
     
-    
     year_preds = year.Infer(img_name=img_path, return_raw=False)
     year_class = year_preds['predicted_class']
 
     return (make_class + ' ' + model_class + year_class)
 
-
+# For main page
 @app.route('/', methods=['GET'])
 def index():
     # Main page
     return render_template('index.html')
 
-
+# For the predictions
 @app.route('/predict', methods=['POST'])
 def upload():
     if request.files.get('file'):
@@ -65,17 +64,14 @@ def upload():
             basepath, 'uploads', secure_filename(f.filename))
         f.save(file_path)
 
-        # print("./uploads/"+secure_filename(f.filename))
         # Make prediction 
         result = model_predict(img_path=file_path)
 
         return result
 
 if __name__ == '__main__':
-    print(("* Loading models and Flask starting server..."
-        "please wait until server has fully started"))
+    print(" * Loading models and Flask starting server... please wait until server has fully started")
     load_models()
 
-    print('Models loaded')
+    print(' * Models loaded')
     app.run(debug=True,host='0.0.0.0',port=5000,threaded=False)
-
